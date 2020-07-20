701b2d1da6838542307cae83fb3deb154880875a
bbo51dog <bbo51dog@gmail.com>
2020-07-20 17:46:03 +0900

    Load plugins

---
+++ src/bbo51dog/sideloader/FolderPluginLoader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace bbo51dog\sideloader;
+
+use pocketmine\plugin\PluginDescription;
+use pocketmine\plugin\PluginLoader;
+use pocketmine\Server;
+
+class FolderPluginLoader implements PluginLoader{
+
+    public function canLoadPlugin(string $path): bool{
+        return is_dir($path) && file_exists($path . "/plugin.yml") && file_exists($path . "/src/");
+    }
+
+    public function loadPlugin(string $file): void{
+        Server::getInstance()->getLoader()->addPath("{$file}/src");
+    }
+
+    public function getPluginDescription(string $file): ?PluginDescription{
+        if(is_dir($file) and file_exists($file . "/plugin.yml")){
+            $yaml = file_get_contents($file . "/plugin.yml");
+            return new PluginDescription($yaml);
+        }
+        return null;
+    }
+
+    public function getAccessProtocol(): string{
+        return '';
+    }
+}
--- src/bbo51dog/sideloader/Main.php
+++ src/bbo51dog/sideloader/Main.php
@@ -2,6 +2,45 @@
 
 namespace bbo51dog\sideloader;
 
-class Main{
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginLoadOrder;
+use pocketmine\utils\Config;
+use function file_exists;
+use function is_dir;
+use function substr;
 
+class Main extends PluginBase{
+
+    public function onLoad(){
+        $config = new Config('SideLoader.yml', Config::YAML, [
+            'dirs' => [
+                'directory/',
+            ],
+            'plugins' => [
+                'ExamplePlugin',
+            ],
+        ]);
+        $loader = new FolderPluginLoader();
+        $this->getServer()->getPluginManager()->registerInterface($loader);
+        /** @var string[] $dirs */
+        $dirs = $config->get('dirs');
+        /** @var string[] $plugins */
+        $plugins = $config->get('plugins');
+        foreach($dirs as $dir){
+            if(substr($dir, -1) !== '/'){
+                $dir .= '/';
+            }
+            foreach($plugins as $k => $plugin){
+                $path = $dir . $plugin;
+                if(file_exists($path) && is_dir($path)){
+                    $this->getServer()->getPluginManager()->loadPlugin($path, [$loader]);
+                    unset($plugins[$k]);
+                }
+            }
+        }
+    }
+
+    public function onEnable(){
+        $this->getServer()->enablePlugins(PluginLoadOrder::STARTUP);
+    }
 }
