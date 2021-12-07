a50006e89b72a695a1ccba5da0c6045be24265d9
bbo51dog <bbo51dog@gmail.com>
2021-12-07 20:50:25 +0900

    PM4.0

--- src/bbo51dog/sideloader/Main.php
+++ src/bbo51dog/sideloader/Main.php
@@ -3,51 +3,26 @@
 namespace bbo51dog\sideloader;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\plugin\PluginEnableOrder;
 use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat as TF;
-use function file_exists;
-use function is_dir;
 
 class Main extends PluginBase{
 
-    public function onLoad(): void {
+    public function onEnable(): void {
         $config = new Config($this->getDataFolder() . 'SideLoader.yml', Config::YAML, [
             'dirs' => [
                 'directory/',
-            ],
-            'plugins' => [
-                'ExamplePlugin',
-            ],
+            ]
         ]);
         $loader = new FolderPluginLoader();
         $this->getServer()->getPluginManager()->registerInterface($loader);
         /** @var string[] $dirs */
         $dirs = $config->get('dirs');
-        /** @var string[] $plugins */
-        $plugins = $config->get('plugins');
-        foreach($dirs as $dir){
-            if(!str_ends_with($dir, '/')){
-                $dir .= '/';
-            }
-            foreach($plugins as $k => $plugin){
-                $path = $dir . $plugin;
-                if(file_exists($path) && is_dir($path)){
-                    $loader->loadPlugin($path);
-                    unset($plugins[$k]);
-                    $this->getLogger()->info(TF::AQUA . "Plugin " . TF::GOLD . $plugin . TF::AQUA . " is successfully loaded from {$path}");
-                }
-            }
+        $loadedPlugins = [];
+        foreach ($dirs as $dir) {
+            $loadedPlugins += $this->getServer()->getPluginManager()->loadPlugins($dir);
         }
-        if(empty($plugins)){
-            return;
+        foreach ($loadedPlugins as $plugin) {
+            $this->getServer()->getPluginManager()->enablePlugin($plugin);
         }
-        foreach($plugins as $plugin){
-            $this->getLogger()->warning("Plugin {$plugin} not found");
-        }
-    }
-
-    public function onEnable(): void {
-        $this->getServer()->enablePlugins(PluginEnableOrder::STARTUP());
     }
 }
